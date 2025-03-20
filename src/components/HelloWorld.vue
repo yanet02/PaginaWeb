c00692698c7ac597ceb1917b18f67a05cbc2df2d
yanet-02 <jenamoradagarcia@gmail.com>
2025-03-19 18:03:54 -0600

    Subiendo proyecto

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,82 @@
+<script setup>
+//metodos
+</script>
+
+<template>
+  <!-- estructura html -->
+  <div class="container">
+
+    <h1 class="text-center">MASCOTAS </h1>
+    <!-- INICIO DEL PRIMER ROW -->
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-header">
+            <h6>Gatos</h6>
+          </div>
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's 
+              content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-header">
+            <h6>Perros</h6>
+          </div>
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's 
+              content.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- CIERRE DEL PRIMER ROW -->
+    <!-- INICIO DEL 2 ROW -->
+     <div class="row mt-4">
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img src="https://nupec.com/wp-content/uploads/2021/02/Captura-de-pantalla-2021-02-08-a-las-13.59.48.png" 
+          class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's 
+              content.</p>
+
+            <button class="btn btn-warning">Más Información</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img src="https://nupec.com/wp-content/uploads/2021/02/Captura-de-pantalla-2021-02-08-a-las-13.59.48.png" 
+          class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's 
+              content.</p>
+
+            <button class="btn btn-warning">Más Información</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img src="https://nupec.com/wp-content/uploads/2021/02/Captura-de-pantalla-2021-02-08-a-las-13.59.48.png" 
+          class="card-img-top" alt="...">
+          <div class="card-body text-center">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's 
+              content.</p>
+
+            <button class="btn btn-warning">Más Información</button>
+          </div>
+        </div>
+      </div>
+     </div>
+    <!-- CIERRE DEL 2 ROW -->
+  </div>
+
+</template>
+
+<style scoped>
+/* estilos css */
+</style>
